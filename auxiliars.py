532e108197181677a8f4283b45853148c03f2049
Nelson Ribeiro Filho <nelsondelimar@gmail.com>
2017-12-16 03:31:29 -0200

    Modified the functions for expanded grid

--- auxiliars.py
+++ auxiliars.py
@@ -6,10 +6,8 @@
 # -----------------------------------------------------------------------------------
 
 import numpy as np # Numpy library
-import scipy as sp
-import pandas as pd
+import scipy as sp # Scipy library
 
-# Auxiliar number 01
 def deg2rad(angle):
     
     '''
@@ -32,7 +30,6 @@ def deg2rad(angle):
     # Return the final output
     return argument
 
-# Auxiliar number 02
 def rad2deg(argument):
     
     '''
@@ -51,8 +48,7 @@ def rad2deg(argument):
     # Return the final output
     return angle
 
-# Auxiliar number 03
-def dircos(inc, dec, azi):
+def dircos(inc, dec):
     
     '''
     This function calculates the cossines projected values on directions using inclination 
@@ -61,8 +57,7 @@ def dircos(inc, dec, azi):
     
     Inputs:
     theta_inc - inclination angle
-    theta_dec - declination angle
-    theta_azi - azimuth angle    
+    theta_dec - declination angle 
     Outputs:
     dirA - projected cossine A
     dirB - projected cossine B
@@ -72,17 +67,15 @@ def dircos(inc, dec, azi):
     # Use the function to convert some values
     incl = deg2rad(inc)
     decl = deg2rad(dec)
-    azim = deg2rad(azi)
-    
+        
     # Calculates the projected cossine values
-    A = np.cos(incl)*np.cos(decl - azim)
-    B = np.cos(incl)*np.sin(decl - azim)
-    C = np.sin(incl)
+    A = np.cos(inc)*np.cos(dec)
+    B = np.cos(inc)*np.sin(dec)
+    C = np.sin(inc)
     
     # Return the final output
     return A, B, C
 
-# Auxiliar number 04
 def regional(field):
     
     '''
@@ -97,32 +90,26 @@ def regional(field):
         valF - float - regional magnetic field value
         incF - float - magnetic field inclination value
         decF - float - magnetic field declination value
-        aziF - float - magnetic field azimuth value    
     Outputs:
     vecF - numpy array - F componentes along X, Y e Z axis
         
     Ps. All inputs can be arrays when they are used for a set of values.    
     '''
     
-    #assert field[0] != 0., 'Value of the regional magnetic field must be nonzero!'
+    assert field[0] != 0., 'Value of the regional magnetic field must be nonzero!'
         
     # Setting the value for the magnetic field
     F = field[0]
     
     # Computes the projected cossine
-    X, Y, Z = dircos(field[1], field[2], field[3])
+    X, Y, Z = dircos(field[1], field[2])
     
     # Compute all components
-    Fx = F*X
-    Fy = F*Y
-    Fz = F*Z
+    Fx, Fy, Fz = F*X, F*Y, F*Z
     
-    # Set the F values as an array
-    vecF =[Fx, Fy, Fz]
-    # Return the final output
-    return vecF
+    # Set F as array and return the output
+    return [Fx, Fy, Fz]
 
-# Auxiliar number 05
 def addnoise(data, std):
     
     '''
@@ -141,7 +128,6 @@ def addnoise(data, std):
     noise = np.random.normal(loc = local, scale = std, size = data.shape)
     return data + noise
 
-# Auxiliar number 06 (a)
 def padzeros(vector, width, ax, kwargs):
     
     '''
@@ -156,56 +142,132 @@ def padzeros(vector, width, ax, kwargs):
     
     Output:
     newvec - numpy array - the new value for the vector
-    
     '''
+
     # Padding with zeros on both axis
     vector[:width[0]] = 0.
     vector[-width[1]:] = 0.
     
+    # Return the final output
     return vector
 
-# Auxiliar number 06 (b)
 def padones(vector, width, ax, kwargs):
     
     '''
-    This function is similar to padzeros functions, but it adds the one value on the axis instead of zeros. It has the same inputs and outputs.
+    This function is similar to padzeros functions, but it adds the one value on the axis instead 
+    of zeros. It has the same inputs and outputs.
     '''
     
     # Padding with zeros on both axis
     vector[:width[0]] = 1.
     vector[-width[1]:] = 1.
     
+    # Return the final output
     return vector
 
-# Auxiliar number 07
-#def pad_data():
-#    
-#    '''
-#
-#    '''
-#    
-#    return padded_data
+def ispower2(data):
+    
+    '''
+    Logical function that states if the data has size or shape in a power of two.
+    If data is a 1D array, it returns True or False. Otherwise if data is a 2D
+    array, it returns a set with (True/False, True/False).
+    
+    Input: 
+    data - numpy array - vector or matrix
+    
+    Output:
+    base - float - number in power of two
+    '''
+    
+    # Verify if data is a vector or a matrix
+    if data.size == data.shape[0]:
+        num = data.size
+        a = num/2.
+        b = num//2.
+        return np.allclose(a,b)
+    elif data.size != data.shape[0]:
+        # To shape(0) calculation
+        num0 = data.shape[0]
+        a, b = num0/2., num0//2.
+        # To shape(1) calculation
+        num1 = data.shape[1]
+        c, d = num1/2., num1//2.
+        return np.allclose(a,b), np.allclose(c,d)
+    
+def nextpower2(num):
+    
+    '''
+    This function is called in the paddata function to stablish which is
+    the next power of two if the grid has no power two in the data set.    
+    
+    Input:
+    num - integer - size of the data shape
+    Output:
+    next - float - next number power of two    
+    
+    Ps. This function was modified from Fatiando A Terra.
+    '''
+    
+    # Asserting some conditions
+    assert num != 0., 'Data shape should be different of zero!'
+    assert num > 0., 'Shape must have no negative values!'
+    
+    # Calculating the next power of 2
+    next = np.ceil(np.log(num)/np.log(2))
+    # Return the final output
+    return int(2**next)
 
-def griddata(x, y, z, shape):
+
+def paddata(data, shape):
+    n = nextpower2(np.max(shape))
+    nx, ny = shape
+    padx = (n - nx)//2
+    pady = (n - ny)//2
+    padded = np.pad(data.reshape(shape),
+                    ((padx, padx), 
+                     (pady, pady)),
+                    mode='edge')
+    return padded
+    
+def datagrid(x, y, zvalues, datashape):
 
     '''
-    This function creates a grid for the data input and interpolate the values using low extrapolate and cubic method.
+    This function creates a grid for the data input and interpolate the values using a 
+    low extrapolate and cubic method. It receives 3 1D array vector (x, y and z) and
+    also the shape
+    
+    Input:
+    x - numpy array - elements for x direction
+    y - numpy array - elements for y direction
+    zvalues - numpy array - values that will be interpolate
+    datashape - float - shape of the data to interpolate
     '''
 
-    area=(x.min(),x.max(),y.min(),y.max())
-    x1,x2,y1,y2=area
-    nx,ny=SHAPE
-    xs=np.linspace(x1,x2,nx)
-    ys=np.linspace(y1,y2,ny)
-    xp,yp=np.meshgrid(ys,xs)[::-1]
-    xp=xp.ravel()
-    yp=yp.ravel()
+    # Setting the area of the data
+    area = [x.min(), x.max(), y.min(), y.max()]
+    # Initial and final values for x and y
+    xi, xf, yi, yf = area
+    # Number of points in x and y directions
+    nx, ny = datashape
+    
+    # Creating the vectors for x and y
+    xn = np.linspace(xi, xf, nx)
+    yn = np.linspace(yi, yf, ny)
+    # Creating the grid
+    xp, yp = np.meshgrid(xn, yn)[::-1]
+    # Eliminatign the values on the edges
+    xp = xp.ravel()
+    yp = yp.ravel()
 
-    extrapolate=True
-    algorithm='cubic' 
-    grid = scipy.interpolate.griddata((x, y), z, (xp, yp),
-                                      method='cubic').ravel()
+    # Allowing the extrapolate of the data
+    # Setting the algorithm
+    extrapolate = True
+    algorithm = 'cubic' 
+    
+    # Calling the Scipy function
+    grid = scipy.interpolate.griddata((x, y), zvalues, (xp, yp), method = 'cubic').ravel()
 
+    # Allowing the extrapolate and also the nearest gridding
     if extrapolate and algorithm != 'nearest' and np.any(np.isnan(grid)):
         if np.ma.is_masked(grid):
             nans = grid.mask
@@ -215,9 +277,11 @@ def griddata(x, y, z, shape):
             grid[nans] = scipy.interpolate.griddata((x[notnans], y[notnans]), grid[notnans],
                                          (x[nans], y[nans]),
                                          method='nearest').ravel()
-    xp=np.reshape(xp,SHAPE)
-    yp=np.reshape(yp,SHAPE)
-    grid=np.reshape(grid,SHAPE)
+    
+    # Setting the values for x, y and z as the same shape
+    xp = xp.reshape(shape)
+    yp = yp.reshape(shape)
+    grid = grid.reshape(shape)
+    
+    # Return the final output
     return(xp,yp,grid)
-
-#def noise_grav(data, std):
