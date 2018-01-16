c7855a0b218b41470b49f7f0427024a9eb7928a4
Nelson Ribeiro Filho <nelsondelimar@gmail.com>
2018-01-16 08:31:38 -0200

    Changes in gravmag codes

--- gravmag.py
+++ gravmag.py
@@ -132,12 +132,13 @@ def sphere_by(x, y, z, sphere, direction):
 
 def sphere_bz(x, y, z, sphere, direction):
     '''    
-    This function is a Python implementation for a Fortran subroutine contained in Blakely (1995). 
-    It computes the Z component of the magnetic induction caused by a sphere with uniform  distribution of
-    magnetization. The direction X represents the north and Z represents growth downward. This function 
-    receives the coordinates of the points of observation (X, Y, Z - arrays), the coordinates of the center 
-    of the sphere (Xe, Ye, Ze), the magnetization intensity M and the values for inclination and declination 
-    (in degrees). The observation values are given in meters.
+    It is a Python implementation for a Fortran subroutine contained in Blakely (1995). It 
+    computes the Z component of the magnetic induction caused by a sphere with uniform  
+    distribution of magnetization. The direction X represents the north and Z represents 
+    growth downward. This function receives the coordinates of the points of observation 
+    (X, Y, Z - arrays), the coordinates of the center of the sphere (Xe, Ye, Ze), the
+    magnetization intensity M and the values for inclination and declination (in degrees). 
+    The observation values are given in meters.
     
     Inputs: 
     x, y, z - numpy arrays - position of the observation points
@@ -149,7 +150,8 @@ def sphere_bz(x, y, z, sphere, direction):
     Outputs:
     Bz - induced field on Z direction
      
-    Ps. The value for Z can be a scalar in the case of one depth, otherwise it can be a set of points.
+    Ps. The value for Z can be a scalar in the case of one depth, otherwise it can be a 
+    set of points.
     '''
     
     # Calculates some constants
