dfd69e864673e0cdcc750703f5d074bc75652e0e
Nelson Ribeiro Filho <nelsondelimar@gmail.com>
2018-01-19 09:39:52 -0200

    Correcting prism functions

--- codes/prism.py
+++ codes/prism.py
@@ -300,37 +300,3 @@ def prism_gz(xo, yo, zo, prism):
     
     # Return the final output
     return gz
-
-def cylinder(xo, zo, model):
-    '''
-    This function is a Python implementation which computes the vertical gravitational attraction due 
-    to a cylinder. The reference is on Blakely (1996).
-    '''
-    
-    # Setting some constants
-    km2m = 0.001
-    G = 6.673e-11
-    si2mGal = 100000.0
-    
-    xc, zc = model[0], model[1]
-    a, rho = model[2], model[3]
-      
-    # Calculates the distances
-    x = xo - xc
-    z = zo - zc
-    # Computing r square
-    r2 = x**2 + z**2
-    
-    # Calculate the constant
-    mass = (np.pi) * (a**2) * rho
-    
-    # Computes gx and gz
-    gx = (-2. * mass * x)/r2
-    gz = (-2. * mass * z)/r2
-    
-    # Conversion to mGal
-    gx *= G*si2mGal*km2m
-    gz *= G*si2mGal*km2m
-    
-    # Return the tinal output
-    return gx, gz
