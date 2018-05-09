dd8a8c2fb2426b53415cd100da6649a9e68a36ee
Nelson Ribeiro Filho <nelsondelimar@gmail.com>
2018-05-09 12:22:49 -0300

    Including some correlation applied tests

---
+++ correlation/codes/derivative.py
@@ -0,0 +1,183 @@
+# -----------------------------------------------------------------------------------
+# Title: Data Filtering
+# Author: Nelson Ribeiro Filho
+# Description: Source codes for potential field filtering
+# Collaborator: Rodrigo Bijani
+# -----------------------------------------------------------------------------------
+
+# Import Python libraries
+from __future__ import division
+import warnings
+import numpy as np
+import auxiliars as aux
+
+def xderiv(x, y, data, n = 1):
+    
+    '''
+    Return the horizontal derivative in x direction for n order in Fourier domain.
+    
+    Inputs:
+    x - numpy 2D array - grid values in x direction
+    y - numpy 2D array - grid values in y direction
+    data - numpy 2D array - potential data
+    n - float - order of the derivative
+    
+    Output:
+    xder - numpy 2D array - derivative in x direction
+    '''
+    
+    # Stablishing some conditions
+    if x.shape != y.shape != data.shape:
+        raise ValueError("All inputs must have same shape!")
+    
+    # Condition for the order of the derivative
+    if n <= 0.:
+        raise ValueError("Order of the derivative must be positive and nonzero!")
+    
+    if n == 0.:
+        res = data
+    else:    
+        # Calculate the wavenuber in x direction
+        kx, _ = aux.wavenumber(x,y)
+        # Apply the Fourier transform
+        xder = np.fft.fft2(data)*((kx*1j)**(n))
+        # Calculating the inverse transform
+        res = np.real(np.fft.ifft2(xder))
+    
+    # Return the final output
+    return res
+
+def yderiv(x, y, data, n = 1):
+    
+    '''
+    Return the horizontal derivative in y direction for n order in Fourier domain.
+
+    Inputs:
+    x - numpy 2D array - grid values in x direction
+    y - numpy 2D array - grid values in y direction
+    data - numpy 2D array - potential data
+    n - float - order of the derivative
+    
+    Output:
+    yder - numpy 2D array - derivative in y direction
+    '''
+    
+    # Stablishing some conditions
+    if x.shape != y.shape != data.shape:
+        raise ValueError("All inputs must have same shape!")
+    
+    # Condition for the order of the derivative
+    if n <= 0.:
+        raise ValueError("Order of the derivative must be positive and nonzero!")
+        
+    if n == 0.:
+        res = data
+    else:    
+        # Calculate the wavenuber in y direction
+        _, ky = aux.wavenumber(x,y)
+    
+        # Apply the Fourier transform
+        yder = np.fft.fft2(data)*((ky*1j)**(n))
+        # Calculate the inverse transform
+        res = np.real(np.fft.ifft2(yder))
+    
+    # Return the final output
+    return res
+
+def zderiv(x, y, data, n = 1):
+    
+    '''
+    Return the vertical derivative in z direction for n order in Fourier domain.
+    
+    Inputs:
+    x - numpy 2D array - grid values in x direction
+    y - numpy 2D array - grid values in y direction
+    data - numpy 2D array - potential data
+    n - float - order of the derivative
+    
+    Output:
+    zder - numpy 2D array - derivative in z direction    
+    '''
+    
+    # Stablishing some conditions
+    if x.shape != y.shape != data.shape:
+        raise ValueError("All inputs must have same shape!")
+    
+    # Condition for the order of the derivative
+    if n < 0.:
+        raise ValueError("Order of the derivative must be positive!")
+    
+    if n == 0.:
+        res = data
+    else:    
+        # Calculate the wavenuber in z direction
+        kx, ky = aux.wavenumber(x,y)
+    
+        # Apply the Fourier transform
+        zder = np.fft.fft2(data)*(np.sqrt(kx**2 + ky**2)**(n))
+    
+        # Calculate the inverse transform
+        res = np.real(np.fft.ifft2(zder))
+    
+    # Return the final output
+    return res
+
+def horzgrad(x, y, data):
+    
+    '''
+    Return the horizontal gradient amplitude (HGA) for a potential data on a regular 
+    grid. All calculation is done by using Fourier domain.
+    
+    Inputs:
+    x - numpy 2D array - grid values in x direction
+    y - numpy 2D array - grid values in y direction
+    data - numpy 2D array - potential data
+    
+    Output:
+    hga - numpy 2D array - horizontal gradient amplitude
+    '''
+    
+    # Stablishing some conditions
+    if x.shape != y.shape != data.shape:
+        raise ValueError("All inputs must have same shape!")
+    
+    # Computes the horizontal derivatives
+    diffx = xderiv(x, y, data)
+    diffy = yderiv(x, y, data)
+    
+    # Calculates the total gradient
+    hga = (diffx**2 + diffy**2)**(0.5)
+    
+    # Return the final output
+    return hga
+
+def totalgrad(x, y, data):
+    
+    '''
+    Return the total gradient amplitude (TGA) for a potential data on a regular grid.
+    
+    Inputs:
+    x - numpy 2D array - grid values in x direction
+    y - numpy 2D array - grid values in y direction
+    data - numpy 2D array - potential data
+    
+    Output:
+    tga - numpy 2D array - total gradient amplitude
+    '''
+    
+    # Stablishing some conditions
+    if x.shape != y.shape != data.shape:
+        raise ValueError("All inputs must have same shape!")
+
+    # Calculates the x derivative
+    diffx = xderiv(x, y, data)
+    # Calculates the y derivative
+    diffy = yderiv(x, y, data)
+    # Calculates the z derivative
+    diffz = zderiv(x, y, data)
+
+    # Calculates the total gradient
+    tga = (diffx**2 + diffy**2 + diffz**2)**(0.5)
+    
+    # Return the final output
+    return tga
---
+++ correlation/codes/statistical.py
@@ -0,0 +1,88 @@
+# -----------------------------------------------------------------------------------
+# Title: Data Filtering
+# Author: Nelson Ribeiro Filho
+# Description: Source codes for potential field filtering
+# Collaborator: Rodrigo Bijani
+# -----------------------------------------------------------------------------------
+
+# Import Python libraries
+from __future__ import division
+import warnings
+import numpy as np
+import warnings
+import auxiliars as aux
+
+def analysis(data, unit=None):
+    
+    '''
+    A statistical function that calculates the minimum and maximum values for a simple 
+    dataset and also its mean values and variations. The dataset can be a 1D or a 2D
+    numpy array.
+    
+    Input:
+    data - numpy array - data set as a vector
+    unit - string - data unit
+    
+    Outputs:
+    datamin - float - minimun value for the data
+    datamax - float - maximun value for the data
+    datamed - float - mean value for all dataset
+    datavar - float - variation for all dataset
+    
+    '''
+    
+    assert data.size > 1, 'Data set must have more than one element!'
+    
+    datamin = np.min(data)
+    datamax = np.max(data)
+    datamed = np.mean(data)
+    datavar = datamax - datamin
+    
+    if (unit != None):
+    	print 'Minimum:    %5.4f' % datamin, unit
+        print 'Maximum:    %5.4f' % datamax, unit
+        print 'Mean value: %5.4f' % datamed, unit
+        print 'Variation:  %5.4f' % datavar, unit
+    else:
+        print 'Minimum:    %5.4f' % datamin
+        print 'Maximum:    %5.4f' % datamax
+        print 'Mean value: %5.4f' % datamed
+        print 'Variation:  %5.4f' % datavar
+        
+    return datamin, datamax, datamed, datavar
+
+def cccoef(data1, data2):
+    
+    '''
+    It returns the simple crosscorrelation coefficient between two data sets, which 
+    can or a single 1D array or a N-dimensional data set. It is very important that 
+    both data sets have the same dimension, otherwise it will runnig the code error.
+    
+    Inputs:
+    data1 - numpy array - first dataset
+    data2 - numpy array - second dataset
+    
+    Output:
+    res - scalar - cross correlation coefficient
+    '''
+    
+    # Stablishing some conditions
+    if data1.shape != data2.shape :
+        raise ValueError("All inputs must have same shape!")
+        
+    # Calculate the simple mean
+    # For the first dataset
+    mean1 = data1.mean()
+    # For the second dataset
+    mean2 = data2.mean()
+       
+    # Formulation
+    numerator = np.sum((data1 - mean1)*(data2 - mean2))
+    den1 = np.sum((data1 - mean1)**2)
+    den2 = np.sum((data2 - mean2)**2)
+    
+    #It calculates the cross correlation coefficient
+    res = numerator/np.sqrt(den1*den2)
+    
+    # Return the final output
+    return res
