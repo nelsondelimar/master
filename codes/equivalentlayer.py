bbda7d78444c93577c0b158938cce49d60451375
Nelson Ribeiro Filho <nelsondelimar@gmail.com>
2018-05-30 14:37:06 -0300

    New functions for sensitivity matrix for gravity tensor elements calculation

--- codes/equivalentlayer.py
+++ codes/equivalentlayer.py
@@ -23,7 +23,7 @@ def layer(area, shape, level):
     '''
     
     # Condition for the level
-    if level < 0 or level == 0:
+    if level < 0. or level == 0.:
         raise ValueError('Depth of the layer must have be positive or non-null')
 
     # Calculates the radius of sphere
@@ -34,7 +34,7 @@ def layer(area, shape, level):
     
     # Create a empty list
     layer = []
-    
+        
     # Includes all source points
     for i in range(len(xo)):
         layer.append([xo[i],yo[i],zo[i],R])
@@ -43,7 +43,7 @@ def layer(area, shape, level):
     return layer
 
 
-def mat_mag(xo, yo, zo, layer, inc, dec, incs, decs):
+def mat_mag_tfa(xo, yo, zo, layer, inc, dec, incs, decs):
     '''
     It calculates the sensitivity matrix for a magnetic case as total field anomaly. It must receives 
     all observation points and the layer model as a list. It also recieves all values of inclination
@@ -65,23 +65,25 @@ def mat_mag(xo, yo, zo, layer, inc, dec, incs, decs):
     cm = 1.e-7
     t2nT = 1.e9
     
-    # Number of points
+    # Number of points:
     n = len(xo)
+    m = len(layer)
+
     # Create the zero-matrix
-    mat = numpy.zeros((n,n))
+    mat = numpy.zeros((n,m))
     
     # Calculate the projections in x, y and z directions
     fx, fy, fz = regional(1., inc, dec)
     mx, my, mz = regional(1., incs, decs)
     
     # Dealing with all kernels at all directions
-    for i,m in enumerate(layer):
-        phi_xx = kernel.kernelxx(xo, yo, zo, m)
-        phi_yy = kernel.kernelyy(xo, yo, zo, m)
-        phi_zz = kernel.kernelzz(xo, yo, zo, m)
-        phi_xy = kernel.kernelxy(xo, yo, zo, m)
-        phi_xz = kernel.kernelxz(xo, yo, zo, m)
-        phi_yz = kernel.kernelyz(xo, yo, zo, m)
+    for i,j in enumerate(layer):
+        phi_xx = kernel.kernelxx(xo, yo, zo, j)
+        phi_yy = kernel.kernelyy(xo, yo, zo, j)
+        phi_zz = kernel.kernelzz(xo, yo, zo, j)
+        phi_xy = kernel.kernelxy(xo, yo, zo, j)
+        phi_xz = kernel.kernelxz(xo, yo, zo, j)
+        phi_yz = kernel.kernelyz(xo, yo, zo, j)
         mat[:,i] = fx*phi_xx*mx + fx*phi_xy*my + fx*phi_xz*mz + \
                    fy*phi_xy*mx + fy*phi_yy*my + fy*phi_yz*mz + \
                    fz*phi_xz*mx + fz*phi_yz*my + fz*phi_zz*mz
@@ -91,7 +93,7 @@ def mat_mag(xo, yo, zo, layer, inc, dec, incs, decs):
     # Return the final output
     return mat
 
-def mat_grav(xo, yo, zo, layer):
+def mat_grav_gz(xo, yo, zo, layer):
     '''
     It calculates the sensitivity matrix for a gravity case as vertical gravitational component. It must 
     receives all observation points and the layer model as a list.
@@ -122,3 +124,44 @@ def mat_grav(xo, yo, zo, layer):
     
     # Return the final output
     return mat
+
+def mat_grav_gz_xyz(xo, yo, zo, layer):
+    '''
+    It calculates the sensitivity matrix for a gravity case as horizontal and vertical derivatives 
+    of the vertical gravitational component. It must receives all observation points and the layer 
+    model as a list.
+    
+    Inputs:
+    xo, yo, zo - numpy array - observation points
+    layer - numpy list - created layer as a list
+    
+    Output:
+    mat_gzx - numpy matrix - first order x-derivative of gz data
+    mat_gzy - numpy matrix - first order y-derivative of gz data
+    mat_gzz - numpy matrix - first order z-derivative of gz data
+    '''
+    # Constants
+    g = 0.00000006673
+    si2mGal = 100000.
+    
+    # Number of points:
+    n = len(xo)
+    m = len(layer)
+    
+    # Create all zero matrix
+    mat_gzx = numpy.zeros((n, m))
+    mat_gzy = numpy.zeros((n, m))
+    mat_gzz = numpy.zeros((n, m))
+    
+    # Calculates all kernels
+    for i,j in enumerate(layer):
+        mat_gzx[:,i] = kernel.kernelxz(xo, yo, zo, j)
+        mat_gzy[:,i] = kernel.kernelyz(xo, yo, zo, j)
+        mat_gzz[:,i] = kernel.kernelzz(xo, yo, zo, j)
+
+    mat_gzx *= g * si2mGal
+    mat_gzy *= g * si2mGal
+    mat_gzz *= g * si2mGal
+    
+    # Return the final output
+    return mat_gzx, mat_gzy, mat_gzz
